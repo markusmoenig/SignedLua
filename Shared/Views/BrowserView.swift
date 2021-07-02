eb7aa83c3097312a36ae18cd60756a76d55bbbc7
Markus Moenig <markus@moenig.io>
2021-07-02 16:42:05 +0700

    Reimplemented browser without NavigationView

--- Shared/Views/BrowserView.swift
+++ Shared/Views/BrowserView.swift
@@ -21,26 +21,90 @@ struct BrowserView: View {
 
     var body: some View {
                 
+        /*
         NavigationView {
+         List {
+             NavigationLink(tag: NavigationItem.shapes, selection: $selection) {
+                 ShapeView(model: model)
+             } label: {
+                 Label("Shapes", systemImage: "square")
+             }
+             
+             NavigationLink(tag: NavigationItem.materials, selection: $selection) {
+             } label: {
+                 Label("Materials", systemImage: "book.closed")
+             }
+             
+             NavigationLink(tag: NavigationItem.scripts, selection: $selection) {
+                 Text("Scripts")
+             } label: {
+                 Label("Scripts", systemImage: "heart")
+             }
+         
+         }
+        }*/
+        
+        HStack {
             List {
-                NavigationLink(tag: NavigationItem.shapes, selection: $selection) {
-                    ShapeView(model: model)
-                } label: {
+                Button(action: {
+                    selection =  .shapes
+                })
+                {
                     Label("Shapes", systemImage: "square")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .contentShape(Rectangle())
+                        .padding(.leading, 6)
+                        .foregroundColor(.white)
                 }
+                .buttonStyle(PlainButtonStyle())
+                .listRowBackground(Group {
+                    if selection == .shapes {
+                        Color.accentColor.mask(RoundedRectangle(cornerRadius: 4))
+                    } else { Color.clear }
+                })
                 
-                NavigationLink(tag: NavigationItem.materials, selection: $selection) {
-                } label: {
-                    Label("Materials", systemImage: "book.closed")
+                Button(action: {
+                    selection =  .materials
+                })
+                {
+                    Label("Materials", systemImage: "light.max")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .contentShape(Rectangle())
+                        .padding(.leading, 6)
+                        .foregroundColor(.white)
                 }
+                .buttonStyle(PlainButtonStyle())
+                .listRowBackground(Group {
+                    if selection == .materials {
+                        Color.accentColor.mask(RoundedRectangle(cornerRadius: 4))
+                    } else { Color.clear }
+                })
                 
-                NavigationLink(tag: NavigationItem.scripts, selection: $selection) {
-                    Text("Scripts")
-                } label: {
-                    Label("Scripts", systemImage: "heart")
+                Button(action: {
+                    selection =  .scripts
+                })
+                {
+                    Label("Scripts", systemImage: "j.square")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .contentShape(Rectangle())
+                        .padding(.leading, 6)
+                        .foregroundColor(.white)
                 }
-            
+                .buttonStyle(PlainButtonStyle())
+                .listRowBackground(Group {
+                    if selection == .scripts {
+                        Color.accentColor.mask(RoundedRectangle(cornerRadius: 4))
+                    } else { Color.clear }
+                })
             }
+            .frame(maxWidth: 130)
+            
+            Divider()
+            
+            if selection == .shapes {
+                ShapeView(model: model)
+            }
+            Spacer()
         }
         
         //.onReceive(model.componentPreviewNeedsUpdate) { _ in
